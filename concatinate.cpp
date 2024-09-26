36fa9bfb80ded72d219a43fd3db5b56db224d789
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-09-26 21:25:40 +0530

    Update concatinate.cpp

--- concatinate.cpp
+++ concatinate.cpp
@@ -1,40 +1,33 @@
 //Wap in c to implement to copy concatinate, compare using steing.h header file.
 #include <stdio.h>
-#include <ctype.h>  
-
+#include <string.h>  
 int main() {
-    char str[100];
-    int vowels = 0, consonants = 0, i = 0;
-
-   
-    printf("Enter a string: ");
-    gets(str);  
-   
-    while (str[i] != '\0') {
-        char ch = tolower(str[i]); 
-
-       
-        if ((ch >= 'a' && ch <= 'z')) {
-           
-            switch (ch) {
-                case 'a':
-                case 'e':
-                case 'i':
-                case 'o':
-                case 'u':
-                    vowels++;
-                    break;
-                default:
-                    consonants++;
-                    break;
-            }
-        }
-        i++;  
-    }
+    char str1[100], str2[100], str3[100];
 
     
-    printf("Number of vowels: %d\n", vowels);
-    printf("Number of consonants: %d\n", consonants);
+    printf("Enter the first string: ");
+    gets(str1);  
+
+    printf("Enter the second string: ");
+    gets(str2);  
+
+    
+    strcpy(str3, str1);
+    printf("\nAfter copying, str3: %s\n", str3);
+
+   
+    strcat(str1, str2);
+    printf("After concatenation, str1: %s\n", str1);
+
+   
+    int comparison = strcmp(str1, str2);
+    if (comparison == 0) {
+        printf("str1 and str2 are equal.\n");
+    } else if (comparison > 0) {
+        printf("str1 is greater than str2.\n");
+    } else {
+        printf("str1 is less than str2.\n");
+    }
 
     return 0;
 }
